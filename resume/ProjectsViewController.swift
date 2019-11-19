7fa41a4393660f371241d2f8aa5ae3008d882256
Chantal Demissie <chantaldemissie@yahoo.com>
2019-11-18 22:57:21 -0800

    project links are now clickable and open urls within app

--- resume/ProjectsViewController.swift
+++ resume/ProjectsViewController.swift
@@ -1,11 +1,17 @@
 import UIKit
 
-class ProjectsViewController: UIViewController, UITableViewDataSource {
+class ProjectsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     var projects = [
         "Dreamlife",
         "Rideshare",
         "Github"
+    ]
+    
+    var links = [
+        "https://dream-life-100.firebaseapp.com/",
+        "http://candr-rideshare.herokuapp.com/",
+        "https://github.com/ChantalDemissie",
     ]
     
     @IBOutlet weak var tableView: UITableView!
@@ -15,10 +21,16 @@ class ProjectsViewController: UIViewController, UITableViewDataSource {
         
         
         tableView.dataSource = self
+        tableView.delegate = self
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return projects.count
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let url =  URL(string: self.links[indexPath.row])!
+        UIApplication.shared.open(url, options: [:])
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -29,5 +41,4 @@ class ProjectsViewController: UIViewController, UITableViewDataSource {
         
         return cell
     }
-    
 }
