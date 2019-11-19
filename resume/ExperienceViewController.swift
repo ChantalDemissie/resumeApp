0bab4a986629c35770c2c806f5a0aacc813735d7
Chantal Demissie <chantaldemissie@yahoo.com>
2019-11-18 18:46:46 -0800

    experience screen

---
+++ resume/ExperienceViewController.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class ExperienceViewController: UIViewController {
+
+    @IBOutlet weak var experiencesLabel: UILabel!
+    
+    var experiences = [
+        "Nordstrom iOS Mobile Team",
+        "Ada Developers Academy"
+    ]
+      
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        experiencesLabel.text = "\u{2022} " + experiences.joined(separator: "\n\u{2022} ")
+    }
+
+}
--- resume/ViewController.swift
+++ resume/ViewController.swift
@@ -1,7 +1,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+  
+    var Languages = ["Swift", "Ruby", "Ruby on Rails",
+                     "React", "Javascript"]
+    
+    var Projects = ["Dreamlife", "rideshare", "github"]
+    
     @IBAction func Experience(_ sender: Any) {
+//        let expButton = sender as! UIButton
+//        let message = "\(experiences)"
+//        expButton.te
     }
     
     @IBAction func Languages(_ sender: Any) {
